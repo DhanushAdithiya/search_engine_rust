88d26274e32c35eb0eb23a8cc11f608d143fd42c
DhanushAdithiya <k.b.dhanush02@gmail.com>
2023-08-11 08:36:16 +0530

    Added Tf-Idf

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,17 @@ mod text_processing;
 #[tokio::main]
 async fn main() {
     // println!("{}", stop_word_removal(String::from("gand and big gaand")));
-    crawl_sites(
-        String::from("https://en.wikipedia.org/wiki/Kingdom_Two_Crowns"),
-        5,
-    )
-    .await;
+    // crawl_sites(
+    //     String::from("https://en.wikipedia.org/wiki/Kingdom_Two_Crowns"),
+    //     5,
+    // )
+    // .await;
+
+    let corpus: Vec<String> = vec![
+        String::from("Hello world"),
+        String::from("Hello world this is a new document"),
+        String::from("Hi how are you?"),
+    ];
+
+    text_processing::tf_idf(corpus);
 }
--- src/text_processing.rs
+++ src/text_processing.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn stop_word_removal(input: String) -> String {
     let stop_words: [&str; 127] = [
         "i",
@@ -141,4 +143,69 @@ pub fn stop_word_removal(input: String) -> String {
     clean
 }
 
-pub fn tf_idf(input: String) {}
+fn calculate_tf(
+    corpus: &Vec<String>,
+    n_words: usize,
+    words: HashSet<String>,
+) -> HashMap<String, f64> {
+    let mut tf_values: HashMap<String, f64> = HashMap::new();
+    for unique in words {
+        let mut count = 0.0;
+        for doc in corpus {
+            for word in doc.split_whitespace() {
+                if word == unique {
+                    count += 1.0;
+                }
+            }
+        }
+
+        let tf = count / n_words as f64;
+        tf_values.insert(unique, tf);
+    }
+    tf_values
+}
+
+fn calculate_idf(
+    corpus: &Vec<String>,
+    n_docs: usize,
+    words: HashSet<String>,
+) -> HashMap<String, f64> {
+    let mut idf_values: HashMap<String, f64> = HashMap::new();
+    for unique in words {
+        let mut count = 0.0;
+        for doc in corpus {
+            if doc.contains(&unique) {
+                count += 1.0;
+            }
+        }
+        let idf = count / n_docs as f64;
+        idf_values.insert(unique, idf);
+    }
+    idf_values
+}
+
+pub fn tf_idf(corpus: Vec<String>) {
+    let mut unique_words: HashSet<String> = HashSet::new();
+    let n_docs = corpus.len();
+
+    for document in corpus.clone() {
+        let document = stop_word_removal(document);
+        for word in document.split_whitespace() {
+            unique_words.insert(String::from(word));
+        }
+    }
+    let n_words = unique_words.len();
+
+    let tf_values = calculate_tf(&corpus, n_words, unique_words.clone());
+    let idf_values = calculate_idf(&corpus, n_docs, unique_words.clone());
+    let mut tf_idf_values: HashMap<String, f64> = HashMap::new();
+
+    for word in tf_values {
+        let value = idf_values.get(&word.0).unwrap();
+        tf_idf_values.insert(word.0, value * word.1);
+    }
+
+    println!("{:?}", tf_idf_values);
+}
+
+//(hello,1.234)
